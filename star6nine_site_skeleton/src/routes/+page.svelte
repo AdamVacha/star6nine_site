7a062e94cba457213150e46adbb3555c7dfe57a4
AdamVacha <adamrvacha@gmail.com>
2024-05-30 21:05:31 -0400

    initial site push v1.0

---
+++ star6nine_site_skeleton/src/routes/+page.svelte
@@ -0,0 +1,63 @@
+<script lang="ts">import { Instagram, YoutubeIcon, TreesIcon } from 'lucide-svelte';
+import { IconBrandSpotify } from '@tabler/icons-svelte';</script>
+
+
+	<!-- ICONS -->
+
+<div class="flex justify-center m-6 gap-4">
+	<a class="btn-icon variant-filled" href="https://www.instagram.com/star6.nine/">
+		<span><Instagram size={36}/></span>
+		
+	</a>
+	<a class="btn-icon variant-filled" href="https://www.youtube.com/channel/UCxhkgu9roj4LMOJNgzNrMpQ">
+		<span><YoutubeIcon size={36}/></span>
+		
+	</a>
+	<a class="btn-icon variant-filled" href="https://open.spotify.com/artist/4PHiEApmqAZgGyCvaJopFb?si=SiCBrCAqSceIw8feccC2hA">
+		<span><IconBrandSpotify size={40}/></span>
+
+	</a>
+	<a class="btn-icon variant-filled" href="https://linktr.ee/ybrthelabel">
+		<span><TreesIcon size={38}/></span>
+
+	</a>
+	
+</div>
+
+
+	<!-- LOGO -->
+<section class="flex justify-center m-10">
+	<div>
+		<enhanced:img src="/home/catty/workspace/star6nine_site/star6nine_site_skeleton/src/lib/assets/star6ninelogo.png" alt="s69 logo" />
+	</div>
+	
+</section>
+
+
+
+	<!-- NOTABLE PERFORMANCES -->
+<!-- <div class="card p-4 m-4 variant-outline-primary">Notable Performances: 
+	Slightly Stoopid - Official Reggae Rise up After Party
+	Eli Mac - Official Reggae Rise Up After Party
+	G - Space & Milano - Floridian Social, St. Petersburg
+	Juju Beats - Floridian Social, St. Petersburg
+</div> -->
+
+	<!-- MUSIC -->
+<!-- <div class="card p-4 m-4 variant-outline-primary">Music
+
+</div> -->
+
+	<!-- WRITEUPS -->
+<!-- <section class="flex justify-center m-10">
+	<div>
+		<enhanced:img src="/home/catty/workspace/star6nine_site/star6nine_site_skeleton/src/lib/assets/press_1s69.png" alt="s69 logo" />
+	</div>
+	
+</section> -->
+
+
+	<!-- BIO -->
+<!-- <div class="card p-4 m-4 variant-outline-primary">BIO - Star6nine is a dubstep project started in 2021 by electronic music producer Adam Vacha. With a lifetime of music theory and composition training *69 was started from scratch. The sounds herein hail from a foreign region of deep space that extends out into a signature dark bass music profile. From the age of 12 learning classical guitar and piano at the Center for the Arts program in St. Petersburg Florida, to the Pinellas County Center for the Arts Program for Highschool and eventually Berklee College of Music for music production, it is safe to say much training has been put into Star6nine’s new iconic sound. 2 years were spent hidden away in recording studios to bring forth the best possible product and now is the time Star6nine hits the stage to show the world it’s new music.
+		With 100’s of hours performing on stage across the country Star6nine is no stranger to live performance or DJ’ing events. Having run a recording studio on La Cienega Blvd in Hollywood CA while DJ’ing raves on the weekends in Los Angeles, not only has the time been put into the studio but the live sets as well. Unique and powerful double drops coupled with driving and extremely deep bass lead to live DJ sets that truly move the crowd. 
+</div>  -->
---
+++ star6nine_site_skeleton/star6ninetheme.ts
@@ -0,0 +1,101 @@
+import type { CustomThemeConfig } from '@skeletonlabs/tw-plugin';
+
+export const star6ninetheme: CustomThemeConfig = {
+	name: 'star6ninetheme',
+	properties: {
+		// =~= Theme Properties =~=
+		'--theme-font-family-base': `system-ui`,
+		'--theme-font-family-heading': `system-ui`,
+		'--theme-font-color-base': '0 0 0',
+		'--theme-font-color-dark': '255 255 255',
+		'--theme-rounded-base': '9999px',
+		'--theme-rounded-container': '8px',
+		'--theme-border-base': '1px',
+		// =~= Theme On-X Colors =~=
+		'--on-primary': '255 255 255',
+		'--on-secondary': '0 0 0',
+		'--on-tertiary': '0 0 0',
+		'--on-success': '0 0 0',
+		'--on-warning': '0 0 0',
+		'--on-error': '0 0 0',
+		'--on-surface': '255 255 255',
+		// =~= Theme Colors  =~=
+		// primary | #7A5028
+		'--color-primary-50': '235 229 223', // #ebe5df
+		'--color-primary-100': '228 220 212', // #e4dcd4
+		'--color-primary-200': '222 211 201', // #ded3c9
+		'--color-primary-300': '202 185 169', // #cab9a9
+		'--color-primary-400': '162 133 105', // #a28569
+		'--color-primary-500': '122 80 40', // #7A5028
+		'--color-primary-600': '110 72 36', // #6e4824
+		'--color-primary-700': '92 60 30', // #5c3c1e
+		'--color-primary-800': '73 48 24', // #493018
+		'--color-primary-900': '60 39 20', // #3c2714
+		// secondary | #A38F54
+		'--color-secondary-50': '241 238 229', // #f1eee5
+		'--color-secondary-100': '237 233 221', // #ede9dd
+		'--color-secondary-200': '232 227 212', // #e8e3d4
+		'--color-secondary-300': '218 210 187', // #dad2bb
+		'--color-secondary-400': '191 177 135', // #bfb187
+		'--color-secondary-500': '163 143 84', // #A38F54
+		'--color-secondary-600': '147 129 76', // #93814c
+		'--color-secondary-700': '122 107 63', // #7a6b3f
+		'--color-secondary-800': '98 86 50', // #625632
+		'--color-secondary-900': '80 70 41', // #504629
+		// tertiary | #aca82a
+		'--color-tertiary-50': '243 242 223', // #f3f2df
+		'--color-tertiary-100': '238 238 212', // #eeeed4
+		'--color-tertiary-200': '234 233 202', // #eae9ca
+		'--color-tertiary-300': '222 220 170', // #dedcaa
+		'--color-tertiary-400': '197 194 106', // #c5c26a
+		'--color-tertiary-500': '172 168 42', // #aca82a
+		'--color-tertiary-600': '155 151 38', // #9b9726
+		'--color-tertiary-700': '129 126 32', // #817e20
+		'--color-tertiary-800': '103 101 25', // #676519
+		'--color-tertiary-900': '84 82 21', // #545215
+		// success | #48ac70
+		'--color-success-50': '228 243 234', // #e4f3ea
+		'--color-success-100': '218 238 226', // #daeee2
+		'--color-success-200': '209 234 219', // #d1eadb
+		'--color-success-300': '182 222 198', // #b6dec6
+		'--color-success-400': '127 197 155', // #7fc59b
+		'--color-success-500': '72 172 112', // #48ac70
+		'--color-success-600': '65 155 101', // #419b65
+		'--color-success-700': '54 129 84', // #368154
+		'--color-success-800': '43 103 67', // #2b6743
+		'--color-success-900': '35 84 55', // #235437
+		// warning | #f90b0b
+		'--color-warning-50': '254 218 218', // #fedada
+		'--color-warning-100': '254 206 206', // #fecece
+		'--color-warning-200': '254 194 194', // #fec2c2
+		'--color-warning-300': '253 157 157', // #fd9d9d
+		'--color-warning-400': '251 84 84', // #fb5454
+		'--color-warning-500': '249 11 11', // #f90b0b
+		'--color-warning-600': '224 10 10', // #e00a0a
+		'--color-warning-700': '187 8 8', // #bb0808
+		'--color-warning-800': '149 7 7', // #950707
+		'--color-warning-900': '122 5 5', // #7a0505
+		// error | #93ab53
+		'--color-error-50': '239 242 229', // #eff2e5
+		'--color-error-100': '233 238 221', // #e9eedd
+		'--color-error-200': '228 234 212', // #e4ead4
+		'--color-error-300': '212 221 186', // #d4ddba
+		'--color-error-400': '179 196 135', // #b3c487
+		'--color-error-500': '147 171 83', // #93ab53
+		'--color-error-600': '132 154 75', // #849a4b
+		'--color-error-700': '110 128 62', // #6e803e
+		'--color-error-800': '88 103 50', // #586732
+		'--color-error-900': '72 84 41', // #485429
+		// surface | #352312
+		'--color-surface-50': '225 222 219', // #e1dedb
+		'--color-surface-100': '215 211 208', // #d7d3d0
+		'--color-surface-200': '205 200 196', // #cdc8c4
+		'--color-surface-300': '174 167 160', // #aea7a0
+		'--color-surface-400': '114 101 89', // #726559
+		'--color-surface-500': '53 35 18', // #352312
+		'--color-surface-600': '48 32 16', // #302010
+		'--color-surface-700': '40 26 14', // #281a0e
+		'--color-surface-800': '32 21 11', // #20150b
+		'--color-surface-900': '26 17 9' // #1a1109
+	}
+};
