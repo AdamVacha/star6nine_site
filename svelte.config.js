873d7af86dd5ece968581207d36f2b0282437152
AdamVacha <adamrvacha@gmail.com>
2025-09-04 15:22:50 -0400

    update 11

--- svelte.config.js
+++ svelte.config.js
@@ -9,10 +9,14 @@ const config = {
 	preprocess: [vitePreprocess()],
 
 	kit: {
-		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
-		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
-		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter()
+		adapter: adapter({
+			// See the adapter documentation for specific options
+			routes: {
+				include: ['/*'],
+				exclude: ['<all>']
+			},
+			fallback: 'plaintext'
+		})
 	}
 };
 export default config;
